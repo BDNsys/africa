258c3202c0f4f21b504ab49f72111ab0a2f61ed0
bereketwag <wagawbereket@gmail.com>
2025-05-18 19:13:00 +0300

    profile editing

--- user/forms.py
+++ user/forms.py
@@ -14,7 +14,12 @@ class CustomAuthenticationForm(AuthenticationForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['full_name', 'email','avatar','phone']
+        fields = ['full_name', 'email', 'avatar', 'phone']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['disabled'] = 'disabled'
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     old_password = forms.CharField(label="Old password", widget=forms.PasswordInput)
